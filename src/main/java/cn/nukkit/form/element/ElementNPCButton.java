eff30ba703b23e6f49f2d565a3181311a5e74baa
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-26 12:11:33 +0900

    use static

--- src/main/java/cn/nukkit/form/element/ElementNPCButton.java
+++ src/main/java/cn/nukkit/form/element/ElementNPCButton.java
@@ -11,15 +11,15 @@ public class ElementNPCButton {
     
     private Object data = null;
     
-    private final int MODE_BUTTON = 0;
-    private final int MODE_ON_CLOSE = 1;
+    private static final int MODE_BUTTON = 0;
+    private static final int MODE_ON_CLOSE = 1;
     private int mode;
     
     private String text;
     
-    private final int TYPE_URL = 0;
-    private final int TYPE_COMMAND = 1;
-    private final int TYPE_INVALID = 2;
+    private static final int TYPE_URL = 0;
+    private static final int TYPE_COMMAND = 1;
+    private static final int TYPE_INVALID = 2;
     private int type;
     
     public ElementNPCButton(String buttonName) {
