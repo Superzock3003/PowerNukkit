a1a90e9fcda90cd3d71388537bc4abe47c58e7ab
good777LUCKY <43497313+good777LUCKY@users.noreply.github.com>
2020-09-26 10:31:35 +0900

    Create FormNPCWindow.java

---
+++ src/main/java/cn/nukkit/form/window/FormNPCWindow.java
@@ -0,0 +1,23 @@
+package cn.nukkit.form.window;
+
+import cn.nukkit.form.response.FormResponse;
+import com.google.gson.Gson;
+
+public abstract class FormNPCWindow {
+
+    private static final Gson GSON = new Gson();
+    
+    protected boolean closed = false;
+    
+    public String getJSONData() {
+        return FormWindow.GSON.toJson(this);
+    }
+    
+    public abstract void setResponse(String data);
+    
+    public abstract FormResponse getResponse();
+    
+    public boolean wasClosed() {
+        return closed;
+    }
+}
