02053b816294bd090c7f3705960b842806d2be25
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 21:10:34 +0900

    Create LayerIsland.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/layer/LayerIsland.java
@@ -0,0 +1,19 @@
+package cn.nukkit.level.biome.v2.layer;
+
+public class LayerIsland extends Layer {
+
+    public LayerIsland() {
+        super();
+    }
+    
+    public int[] generate(Layer layer, int x, int z, int width, int height) {
+        long startSeed = layer.getStartSeed();
+        long chunkSeed;
+        
+        for (int j = 0; j < height; j++) {
+            for (int i = 0; i < width; i++) {
+                
+            }
+        }
+    }
+}
