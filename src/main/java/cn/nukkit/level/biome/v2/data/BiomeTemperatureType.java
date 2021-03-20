66fc39b7bfe57130228b7cacc4bb11446381abe7
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 15:03:14 +0900

    Create BiomeTemperatureType.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/data/BiomeTemperatureType.java
@@ -0,0 +1,14 @@
+package cn.nukkit.level.biome.v2.data;
+
+/**
+ * @author GoodLucky777
+ */
+public enum BiomeTemperatureType {
+
+    OCEANIC,
+    WARM,
+    LUSH,
+    COLD,
+    FREEZING,
+    SPECIAL;
+}
