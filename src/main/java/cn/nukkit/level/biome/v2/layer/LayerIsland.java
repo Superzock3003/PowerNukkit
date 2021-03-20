b1e28c0a923b96b1f51d969488b9613fc1274d8f
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 22:35:39 +0900

    update

--- src/main/java/cn/nukkit/level/biome/v2/layer/LayerIsland.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/LayerIsland.java
@@ -10,13 +10,22 @@ public class LayerIsland extends Layer {
     }
     
     public int[] generate(Layer layer, int x, int z, int width, int height) {
+        int[] values = new int[width * height];
+        
         long startSeed = layer.getStartSeed();
         long chunkSeed;
         
         for (int j = 0; j < height; j++) {
             for (int i = 0; i < width; i++) {
-                
+                chunkSeed = LayerHelper.getChunkSeed(startSeed, i + x, j + z);
+                values[i + (j * width)] = LayerHelper.mcFirstIsZero(chunkSeed, 10);
             }
         }
+        
+        if (x > -width && x <= 0 && z > -height && z <= 0) {
+            values[-x + (-z * width)] = 1;
+        }
+        
+        return values;
     }
 }
