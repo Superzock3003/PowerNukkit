09d460a1e441670b2a40061b25576e2f5336fe2e
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 20:59:07 +0900

    Create Layer.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/Layer.java
@@ -0,0 +1,14 @@
+package cn.nukkit.level.biome.v2.layer;
+
+public class Layer {
+
+    private final long layerSalt;
+    private final long startSalt;
+    private final long startSeed;
+    //void *noise;
+    //void *data;
+    private final int scale;
+    private final int edge;
+    //int (*getMap)(const Layer *, int *, int, int, int, int);
+    private final Layer parent, parent2;
+}
