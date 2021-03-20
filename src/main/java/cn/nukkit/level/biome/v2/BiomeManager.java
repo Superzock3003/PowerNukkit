52b7792be151895f0d31007cc7a2badc277ef356
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 14:45:19 +0900

    Create BiomeManager.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/BiomeManager.java
@@ -0,0 +1,21 @@
+package cn.nukkit.level.biome.v2;
+
+/**
+ * @author GoodLucky777
+ */
+public class BiomeManager {
+
+    public static final int MAX_BIOME_ID = 256;
+    
+    public Biome[] biomes = new Biome[MAX_BIOME_ID];
+    
+    public void init() {
+        
+    }
+    
+    public void registerBiome(int id, float height, float temperature, BiomeType type, BiomeTemperatureType temperatureType) {
+        Preconditions.checkArgument(id >= 0 && id < MAX_BIOME_ID, "Biome ID " + id + " is out of range");
+        
+        biomes[id] = new Biome(id, height, temperature, type, temperatureType);
+    }
+}
