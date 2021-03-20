d5df5090624a84211ce044c674e1ec09a5989aaf
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-20 15:00:17 +0900

    Create BiomeType.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/data/BiomeType.java
@@ -0,0 +1,26 @@
+package cn.nukkit.level.biome.v2.data;
+
+/**
+ * @author GoodLucky777
+ */
+public enum BiomeType {
+
+    NONE,
+    OCEAN,
+    PLAINS,
+    DESERT,
+    EXTREME_HILLS,
+    FOREST,
+    TAIGA,
+    SWAMP,
+    RIVER,
+    NETHER,
+    THEEND,
+    ICY,
+    MUSHROOM,
+    BEACH,
+    JUNGLE,
+    STONEBEACH,
+    SAVANNA,
+    MESA;
+}
