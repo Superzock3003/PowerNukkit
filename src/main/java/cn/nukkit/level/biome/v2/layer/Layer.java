8bb54faaccff3faf70f21724356f4c3ffb1cb30f
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-21 09:56:31 +0900

    improve

--- src/main/java/cn/nukkit/level/biome/v2/layer/Layer.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/Layer.java
@@ -1,17 +1,22 @@
 package cn.nukkit.level.biome.v2.layer;
 
+import lombok.Getter;
+import lombok.Setter;
+
 /**
  * @author GoodLucky777
  */
+@Getter
+@Setter
 public class Layer {
 
-    private final long layerSalt;
-    private final long startSalt;
-    private final long startSeed;
+    private long layerSalt;
+    private long startSalt;
+    private long startSeed;
     //void *noise;
     //void *data;
-    private final int scale;
-    private final int edge;
+    private int scale;
+    private int edge;
     //int (*getMap)(const Layer *, int *, int, int, int, int);
-    private final Layer parent, parent2;
+    private Layer parent, parent2;
 }
