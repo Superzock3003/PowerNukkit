84dd1f910e020f8415c461abecd37ea4850b63a0
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-21 10:23:29 +0900

    Create LayerType.java

---
+++ src/main/java/cn/nukkit/level/biome/v2/data/LayerType.java
@@ -0,0 +1,21 @@
+package cn.nukkit.level.biome.v2.data;
+
+/**
+ * @author GoodLucky777
+ */
+public interface LayerType {
+
+    int ISLAND_4096 = 0;
+    int ZOOM_2048 = 1;
+    int ADD_ISLAND_2048 = 2;
+    int ZOOM_1024 = 3;
+    int ADD_ISLAND_1024A = 4;
+    int ADD_ISLAND_1024B = 5;
+    int ADD_ISLAND_1024C = 6;
+    int REMOVE_OCEAN_1024 = 7;
+    int ADD_SNOW_1024 = 8;
+    int ADD_ISLAND_1024D = 9;
+    int COOL_WARM_1024 = 10;
+    int HEAT_ICE_1024 = 11;
+    int SPECIAL_1024 = 12;
+}
