1481b239761bb3df67c311da51abf5e829c65b7c
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-21 09:48:16 +0900

    update

--- src/main/java/cn/nukkit/level/biome/v2/layer/LayerManager.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/LayerManager.java
@@ -9,5 +9,16 @@ public class LayerManager {
         
     }
     
-    
+    public void registerLayer(Layer layer, Layer parent, int salt) {
+        layer.setSalt = salt > 0 ? getLayerSalt(salt) : salt;
+        layer.setStartSalt = 0;
+        layer.setStartSeed = 0;
+        layer.setParnet = parent;
+        layer.setParent2 = null;
+        layer.setScale = 0;
+        layer.setEdge = 0;
+        layer.setGetMap = getMap;
+        layer.setNoise = null;
+        layer.setData = null;
+    }
 }
