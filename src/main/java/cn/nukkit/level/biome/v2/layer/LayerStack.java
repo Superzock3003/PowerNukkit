b4ec51b0d65214aa4720b3fedaf76402154aa12f
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-21 10:32:05 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/v2/layer/LayerStack.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/LayerStack.java
@@ -1,6 +1,7 @@
 package cn.nukkit.level.biome.v2.layer;
 
 import cn.nukkit.level.biome.v2.data.LayerType;
+import cn.nukkit.level.generator.noise.vanilla.f.NoiseGeneratorPerlinF;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -10,10 +11,10 @@ import lombok.Setter;
  */
 @Getter
 @Setter
-public class Layer implements LayerType {
+public class LayerStack implements LayerType {
 
     private Layer[] layers = new Layer[LAYER_COUNT];
     private Layer entry_1;
     private Layer entry_4;
-    //private PerlinNoise oceanRnd;
+    private NoiseGeneratorPerlinF oceanRnd;
 }
