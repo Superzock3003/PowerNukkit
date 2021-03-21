11362fc3dbb68eccb6252259241d99d56933d300
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-21 10:18:52 +0900

    update

--- src/main/java/cn/nukkit/level/biome/v2/layer/LayerManager.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/LayerManager.java
@@ -1,12 +1,16 @@
 package cn.nukkit.level.biome.v2.layer;
 
+import cn.nukkit.level.biome.v2.data.LayerType;
+
 /**
  * @author GoodLucky777
  */
-public class LayerManager {
+public class LayerManager implements LayerType {
 
+    private Layer[] layers;
+    
     public void init() {
-        
+        this.registerVanillaLayers();
     }
     
     public void registerLayer(Layer layer, Layer parent, int salt) {
@@ -17,8 +21,17 @@ public class LayerManager {
         layer.setParent2 = null;
         layer.setScale = 0;
         layer.setEdge = 0;
-        layer.setGetMap = getMap;
-        layer.setNoise = null;
-        layer.setData = null;
+        //layer.setGetMap = getMap;
+        //layer.setNoise = null;
+        //layer.setData = null;
+    }
+    
+    public void registerVanillaLayers() {
+        LayerStack layerStack = new LayerStack();
+        layers = layerStack.getLayers();
+        
+        this.registerLayer(layers[ISLAND_4096], null, 1);
+        this.registerLayer(layers[ZOOM_2048], layers[ISLAND_4096], 2000);
+        
     }
 }
