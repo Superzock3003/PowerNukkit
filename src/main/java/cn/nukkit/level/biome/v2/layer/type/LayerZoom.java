ee7395875b0baf1a7c9439c371c00445f4ada0e6
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-26 10:48:12 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/v2/layer/type/LayerZoom.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/type/LayerZoom.java
@@ -14,24 +14,29 @@ public class LayerZoom extends Layer {
         int pZ = z >> 1;
         int pW = (x + w >> 1) - pX + 1; // (w >> 1) + 2;
         int pH = (z + h >> 1) - pZ + 1; // (h >> 1) + 2;
+        if (this.getParent() == null) {
+            Preconditions.checkNotNull(this.getParent(), "Parent should not be null.");
+        } else {
+            parentValues = this.getParent().generateBiomeValues(pX, pZ, pW, pH);
+        }
         int newW = pW << 1;
         int newH = pH << 1;
         int idx, v00, v01, v10, v11;
-        int[] values = new int[(newW + 1) * (newH + 1)];
+        int[] buf = new int[(newW + 1) * (newH + 1)];
         final int st = this.getStartSalt();
         final int ss = this.getStartSeed();
         for(int j = 0; j < pH; j++) {
             idx = (j << 1) * newW;
-            v00 = values[(j + 0) * pW];
-            v01 = values[(j + 1) * pW];
+            v00 = parentValues[(j + 0) * pW];
+            v01 = parentValues[(j + 1) * pW];
             for(int i = 0; i < pW; i++, v00 = v10, v01 = v11) {
-                v10 = out.get(i + 1 + (j + 0) * pW);
-                v11 = out.get(i + 1 + (j + 1) * pW);
+                v10 = parentValues[i + 1 + (j + 0) * pW];
+                v11 = parentValues[i + 1 + (j + 1) * pW];
                 if(v00 == v01 && v00 == v10 && v00 == v11) {
-                    values[idx] = v00;
-                    values[idx + 1] = v00;
-                    values[idx + newW] = v00;
-                    values[idx + newW + 1] = v00;
+                    buf[idx] = v00;
+                    buf[idx + 1] = v00;
+                    buf[idx + newW] = v00;
+                    buf[idx + newW + 1] = v00;
                     idx += 2;
                     continue;
                 }
@@ -45,19 +50,22 @@ public class LayerZoom extends Layer {
                 cs += chunkX;
                 cs *= cs * 1284865837 + (int)4150755663L;
                 cs += chunkZ;
-                values[idx] = v00;
-                values[idx + newW] = (cs >> 24 & 1) != 0 ? v01 : v00;
+                buf[idx] = v00;
+                buf[idx + newW] = (cs >> 24 & 1) != 0 ? v01 : v00;
                 idx++;
                 cs *= cs * 1284865837 + (int)4150755663L;
                 cs += st;
-                values[idx] = (cs >> 24 & 1) != 0 ? v10 : v00;
-                values[idx + newW] = 10;
+                buf[idx] = (cs >> 24 & 1) != 0 ? v10 : v00;
+                buf[idx + newW] = 10;
                 idx++;
             }
         }
+        
+        int[] values = new int[width * height];
         for(int j = 0; j < h; j++) {
-            nnc(nnc(out).shift(j * w)).copyFrom(buf.shift((j + (z & 1)) * newW + (x & 1)), w);
+            System.arraycopy(buf, (j + (z & 1)) * newW + (x & 1), values, j * width, width);
         }
+        
         return values;
     }
     
