c7c477b54ced752d3615bf32f3d0fdbc6b77c0f5
GoodLucky777 <43497313+GoodLucky777@users.noreply.github.com>
2021-03-26 10:50:25 +0900

    fix

--- src/main/java/cn/nukkit/level/biome/v2/layer/type/LayerZoom.java
+++ src/main/java/cn/nukkit/level/biome/v2/layer/type/LayerZoom.java
@@ -3,6 +3,8 @@ package cn.nukkit.level.biome.v2.layer.type;
 import cn.nukkit.level.biome.v2.layer.Layer;
 import cn.nukkit.level.biome.v2.layer.LayerHelper;
 
+import com.google.common.base.Preconditions;
+
 /**
  * @author GoodLucky777
  */
@@ -70,22 +72,24 @@ public class LayerZoom extends Layer {
     }
     
     private static int select4(int cs, int st, int v00, int v01, int v10, int v11) {
-        int v;
+        int value;
         int cv00 = (v00 == v10 ? 1 : 0) + (v00 == v01 ? 1 : 0) + (v00 == v11 ? 1 : 0);
         int cv10 = (v10 == v01 ? 1 : 0) + (v10 == v11 ? 1 : 0);
         int cv01 = v01 == v11 ? 1 : 0;
+        
         if(cv00 > cv10 && cv00 > cv01) {
-            v = v00;
+            value = v00;
         } else if(cv10 > cv00) {
-            v = v10;
+            value = v10;
         } else if(cv01 > cv00) {
-            v = v01;
+            value = v01;
         } else {
             cs *= cs * 1284865837 + (int)4150755663L;
             cs += st;
             int r = cs >> 24 & 3;
-            v = r == 0 ? v00 : r == 1 ? v10 : r == 2 ? v01 : v11;
+            value = r == 0 ? v00 : r == 1 ? v10 : r == 2 ? v01 : v11;
         }
-        return v;
+        
+        return value;
     }
 }
